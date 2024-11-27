d6444ba1e82f9075612e908a7377c4843c09f5af
SuperPyroManiac <1407314+SuperPyroManiac@users.noreply.github.com>
2024-11-27 13:29:15 -0700

    Working context menu command

---
+++ src/Commands/ValidateFiles.ts
@@ -0,0 +1,23 @@
+import { Command } from '@sapphire/framework';
+import { ApplicationCommandType, ContextMenuCommandType } from 'discord.js';
+
+export class SlashCommand extends Command {
+  public constructor(context: Command.LoaderContext, options: Command.Options) {
+    super(context, {
+      ...options,
+      description: 'Delete message and ban author.',
+    });
+  }
+
+  public override registerApplicationCommands(registry: Command.Registry) {
+    registry.registerContextMenuCommand((builder) =>
+      builder //
+        .setName('Validate Files')
+        .setType(ApplicationCommandType.Message as ContextMenuCommandType)
+    );
+  }
+
+  public override contextMenuRun(interaction: Command.ContextMenuCommandInteraction) {
+    return interaction.reply(`Interaction works, ran by ${interaction.user.tag}`);
+  }
+}
