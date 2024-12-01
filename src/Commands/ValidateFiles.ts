ec3f8dc0d5801efcf270dcae99dc5911314c68f8
SuperPyroManiac <1407314+SuperPyroManiac@users.noreply.github.com>
2024-11-30 19:48:51 -0700

    feat: Enhance logging functionality and file validation

--- src/Commands/ValidateFiles.ts
+++ src/Commands/ValidateFiles.ts
@@ -1,5 +1,7 @@
 import { Command } from '@sapphire/framework';
-import { ApplicationCommandType, ContextMenuCommandType } from 'discord.js';
+import { ApplicationCommandType, Attachment, ContextMenuCommandType, Message } from 'discord.js';
+import { EmbedCreator } from '../Functions/Messages/EmbedCreator';
+import { Logger } from '../Functions/Messages/Logger';
 
 export class ValidateFilesCommand extends Command {
   public constructor(context: Command.LoaderContext, options: Command.Options) {
@@ -17,10 +19,37 @@ export class ValidateFilesCommand extends Command {
     );
   }
 
-  public override contextMenuRun(interaction: Command.ContextMenuCommandInteraction) {
-    return interaction.reply({
-      content: `Interaction works, ran by ${interaction.user.tag}`,
-      ephemeral: true,
-    });
+  public override async contextMenuRun(interaction: Command.ContextMenuCommandInteraction) {
+    const targetMessage: Message = await interaction.channel!.messages.fetch(interaction.targetId);
+    const acceptedTypes = ['RagePluginHook', 'ELS', 'asiloader', 'ScriptHookVDotNet', '.xml', '.meta'];
+    let attach: Attachment;
+
+    //! Attachment Processing
+    if (targetMessage.attachments.size === 0) {
+      // prettier-ignore
+      await interaction.reply({embeds: [EmbedCreator.Error('__No File Found!__\r\n>>> The selected message must include a valid log type!\r\n- RagePluginHook.log\r\n- ELS.log\r\n- ScriptHookVDotNet.log\r\n- asiloader.log')]});
+      return;
+    } else if (targetMessage.attachments.size === 1) {
+      attach = targetMessage.attachments.first()!;
+      if (attach.size / 1000000 > 10) {
+        // prettier-ignore
+        await interaction.reply({embeds: [EmbedCreator.Error('__Blacklisted!__\r\n>>> You have sent a log bigger than 10MB! Your access to the bot has been revoked. You can appeal this at https://dsc.PyrosFun.com')]});
+        // prettier-ignore
+        Logger.UserLog(EmbedCreator.Warning(`__Possible Abuse__\r\n
+          >>> **User:** ${interaction.user.tag} (${interaction.user.id})\r\n
+           **File Size:** ${attach.size / 1000000}MB\r\n
+           **Server:** ${interaction.guild?.name} (${interaction.guild?.id})\r\n
+           **Channel:** ${() => {if (!interaction.channel!.isDMBased()){interaction.channel!.name;}}}\r\n
+           User sent a log greater than 3MB!`), attach)
+        //TODO: ADD BLACKLIST FUNCTION
+        return;
+      } else if (attach.size / 1000000 > 3) {
+        // prettier-ignore
+        await interaction.reply({embeds: [EmbedCreator.Warning('__Skipped!__\r\n>>> You have sent a log bigger than 3MB! We do not support logs greater than 3MB.')]});
+        // prettier-ignore
+        Logger.UserLog(EmbedCreator.Warning(`__Possible Abuse__\r\n>>> **User:** ${interaction.user.tag} (${interaction.user.id})\r\n**File Size:** ${attach.size / 1000000}MB\r\n**Server:** ${interaction.guild?.name} (${interaction.guild?.id})\r\n**Channel:** ${interaction.channel?.id}\r\nUser sent a log greater than 3MB!`), attach)
+        return;
+      }
+    }
   }
 }
--- src/Functions/Messages/Logger.ts
+++ src/Functions/Messages/Logger.ts
@@ -1,35 +1,67 @@
-import { EmbedBuilder } from '@discordjs/builders';
+import { EmbedBuilder, hyperlink } from '@discordjs/builders';
 import { container } from '@sapphire/framework';
+import { codeBlock } from '@sapphire/utilities';
+import { Attachment } from 'discord.js';
 //TODO: Add loggers for missing plugins, blame, etc. Totally overhaul this garbage.
 
 export abstract class Logger {
   static async ErrLog(message: string) {
     const ch = container.client.channels.cache.get(process.env.ERROR_LOG_CHANNEL!);
-    if (message.length > 200) {
-      if (ch?.isSendable()) await ch.send(`### __Error Logged__\r\n\`\`\`${message.substring(0, 1850)}\`\`\``);
+    if (message.length >= 2000) {
+      if (ch?.isSendable()) await ch.send(`### __Error Logged__\r\n${codeBlock(message.substring(0, 1850))}`);
       return;
     }
-    if (ch?.isSendable()) await ch.send(`### __Error Logged__\r\n\`\`\`${message}\`\`\``);
+    if (ch?.isSendable()) await ch.send(`### __Error Logged__\r\n${codeBlock(message)}`);
   }
 
-  static async BotLog(message: EmbedBuilder | string): Promise<void> {
+  static async BotLog(message: EmbedBuilder | string, attachment?: Attachment): Promise<void> {
     const ch = container.client.channels.cache.get(process.env.BOT_LOG_CHANNEL!);
-
-    if (ch?.isSendable() && typeof message === 'string') await ch.send(message);
-    else if (ch?.isSendable() && message instanceof EmbedBuilder) await ch.send({ embeds: [message] });
+    if (attachment && attachment.size / 1000000 > 24) {
+      if (typeof message === 'string') message == message + `\r\nFile Too Large To Upload: ${hyperlink('Link', attachment.url)}`;
+      else if (message instanceof EmbedBuilder)
+        message.setDescription(message.data.description + `\r\nFile Too Large To Upload: ${hyperlink('Link', attachment.url)}`);
+      attachment = undefined;
+    }
+    if (attachment) {
+      if (ch?.isSendable() && typeof message === 'string') await ch.send({ content: message, files: [attachment] });
+      else if (ch?.isSendable() && message instanceof EmbedBuilder) await ch.send({ embeds: [message], files: [attachment] });
+    } else {
+      if (ch?.isSendable() && typeof message === 'string') await ch.send(message);
+      else if (ch?.isSendable() && message instanceof EmbedBuilder) await ch.send({ embeds: [message] });
+    }
   }
 
-  static async ServerLog(message: EmbedBuilder | string): Promise<void> {
+  static async ServerLog(message: EmbedBuilder | string, attachment?: Attachment): Promise<void> {
     const ch = container.client.channels.cache.get(process.env.SERVER_LOG_CHANNEL!);
-
-    if (ch?.isSendable() && typeof message === 'string') await ch.send(message);
-    else if (ch?.isSendable() && message instanceof EmbedBuilder) await ch.send({ embeds: [message] });
+    if (attachment && attachment.size / 1000000 > 24) {
+      if (typeof message === 'string') message == message + `\r\nFile Too Large To Upload: ${hyperlink('Link', attachment.url)}`;
+      else if (message instanceof EmbedBuilder)
+        message.setDescription(message.data.description + `\r\nFile Too Large To Upload: ${hyperlink('Link', attachment.url)}`);
+      attachment = undefined;
+    }
+    if (attachment) {
+      if (ch?.isSendable() && typeof message === 'string') await ch.send({ content: message, files: [attachment] });
+      else if (ch?.isSendable() && message instanceof EmbedBuilder) await ch.send({ embeds: [message], files: [attachment] });
+    } else {
+      if (ch?.isSendable() && typeof message === 'string') await ch.send(message);
+      else if (ch?.isSendable() && message instanceof EmbedBuilder) await ch.send({ embeds: [message] });
+    }
   }
 
-  static async UserLog(message: EmbedBuilder | string): Promise<void> {
+  static async UserLog(message: EmbedBuilder | string, attachment?: Attachment): Promise<void> {
     const ch = container.client.channels.cache.get(process.env.BOT_LOG_CHANNEL!);
-
-    if (ch?.isSendable() && typeof message === 'string') await ch.send(message);
-    else if (ch?.isSendable() && message instanceof EmbedBuilder) await ch.send({ embeds: [message] });
+    if (attachment && attachment.size / 1000000 > 24) {
+      if (typeof message === 'string') message == message + `\r\nFile Too Large To Upload: ${hyperlink('Link', attachment.url)}`;
+      else if (message instanceof EmbedBuilder)
+        message.setDescription(message.data.description + `\r\nFile Too Large To Upload: ${hyperlink('Link', attachment.url)}`);
+      attachment = undefined;
+    }
+    if (attachment) {
+      if (ch?.isSendable() && typeof message === 'string') await ch.send({ content: message, files: [attachment] });
+      else if (ch?.isSendable() && message instanceof EmbedBuilder) await ch.send({ embeds: [message], files: [attachment] });
+    } else {
+      if (ch?.isSendable() && typeof message === 'string') await ch.send(message);
+      else if (ch?.isSendable() && message instanceof EmbedBuilder) await ch.send({ embeds: [message] });
+    }
   }
 }
