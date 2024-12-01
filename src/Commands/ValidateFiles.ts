66d4d14957e4262a70d45726438fdd7b023a3d92
SuperPyroManiac <1407314+SuperPyroManiac@users.noreply.github.com>
2024-12-01 00:24:39 -0700

    feat: Implement RPH file validation and improve error handling

--- src/Commands/ValidateFiles.ts
+++ src/Commands/ValidateFiles.ts
@@ -2,32 +2,25 @@ import { Command } from '@sapphire/framework';
 import { ApplicationCommandType, Attachment, ContextMenuCommandType, Message } from 'discord.js';
 import { EmbedCreator } from '../Functions/Messages/EmbedCreator';
 import { Logger } from '../Functions/Messages/Logger';
+import { RPHValidator } from '../Processors/RPH/RPHValidator';
 
 export class ValidateFilesCommand extends Command {
   public constructor(context: Command.LoaderContext, options: Command.Options) {
-    super(context, {
-      ...options,
-      description: 'Validate the selected files.',
-    });
+    super(context, { ...options, description: 'Validate the selected files.' });
   }
 
   public override registerApplicationCommands(registry: Command.Registry) {
-    registry.registerContextMenuCommand((builder) =>
-      builder //
-        .setName('Validate Files')
-        .setType(ApplicationCommandType.Message as ContextMenuCommandType)
-    );
+    registry.registerContextMenuCommand((builder) => builder.setName('Validate Files').setType(ApplicationCommandType.Message as ContextMenuCommandType));
   }
 
   public override async contextMenuRun(interaction: Command.ContextMenuCommandInteraction) {
     const targetMessage: Message = await interaction.channel!.messages.fetch(interaction.targetId);
-    const acceptedTypes = ['RagePluginHook', 'ELS', 'asiloader', 'ScriptHookVDotNet', '.xml', '.meta'];
-    let attach: Attachment;
+    const acceptedTypes = ['ragepluginhook', 'els', 'asiloader', 'scripthookvdotnet', '.xml', '.meta'];
+    let attach: Attachment | undefined;
 
-    //! Attachment Processing
     if (targetMessage.attachments.size === 0) {
       // prettier-ignore
-      await interaction.reply({embeds: [EmbedCreator.Error('__No File Found!__\r\n>>> The selected message must include a valid log type!\r\n- RagePluginHook.log\r\n- ELS.log\r\n- ScriptHookVDotNet.log\r\n- asiloader.log')]});
+      await interaction.reply({embeds: [EmbedCreator.Error('__No File Found!__\r\n>>> The selected message must include a valid log type!\r\n- RagePluginHook.log\r\n- ELS.log\r\n- ScriptHookVDotNet.log\r\n- asiloader.log\r\n- .xml\r\n- .meta')], ephemeral: true});
       return;
     } else if (targetMessage.attachments.size === 1) {
       attach = targetMessage.attachments.first()!;
@@ -35,12 +28,7 @@ export class ValidateFilesCommand extends Command {
         // prettier-ignore
         await interaction.reply({embeds: [EmbedCreator.Error('__Blacklisted!__\r\n>>> You have sent a log bigger than 10MB! Your access to the bot has been revoked. You can appeal this at https://dsc.PyrosFun.com')]});
         // prettier-ignore
-        Logger.UserLog(EmbedCreator.Warning(`__Possible Abuse__\r\n
-          >>> **User:** ${interaction.user.tag} (${interaction.user.id})\r\n
-           **File Size:** ${attach.size / 1000000}MB\r\n
-           **Server:** ${interaction.guild?.name} (${interaction.guild?.id})\r\n
-           **Channel:** ${() => {if (!interaction.channel!.isDMBased()){interaction.channel!.name;}}}\r\n
-           User sent a log greater than 3MB!`), attach)
+        Logger.UserLog(EmbedCreator.Warning(`__Possible Abuse__\r\n>>> **User:** ${interaction.user.tag} (${interaction.user.id})\r\n**File Size:** ${attach.size / 1000000}MB\r\n**Server:** ${interaction.guild?.name} (${interaction.guild?.id})\r\n**Channel:** ${() => {if (!interaction.channel!.isDMBased()){interaction.channel!.name;}}}\r\nUser sent a log greater than 3MB!`), attach)
         //TODO: ADD BLACKLIST FUNCTION
         return;
       } else if (attach.size / 1000000 > 3) {
@@ -50,6 +38,31 @@ export class ValidateFilesCommand extends Command {
         Logger.UserLog(EmbedCreator.Warning(`__Possible Abuse__\r\n>>> **User:** ${interaction.user.tag} (${interaction.user.id})\r\n**File Size:** ${attach.size / 1000000}MB\r\n**Server:** ${interaction.guild?.name} (${interaction.guild?.id})\r\n**Channel:** ${interaction.channel?.id}\r\nUser sent a log greater than 3MB!`), attach)
         return;
       }
+    } else if (targetMessage.attachments.size > 1) {
+      // prettier-ignore
+      await interaction.reply({embeds: [EmbedCreator.Error('__Multiple Files Found!__\r\n>>> The selected message must include only a single valid log type! The multi selector is implimented yet.')]});
+      return;
+    }
+
+    if (!attach) {
+      // prettier-ignore
+      await interaction.reply({embeds: [EmbedCreator.Error('__No File Found!__\r\n>>> The selected message must include a valid log type!\r\n- RagePluginHook.log\r\n- ELS.log\r\n- ScriptHookVDotNet.log\r\n- asiloader.log\r\n- .xml\r\n- .meta')], ephemeral: true});
+      return;
+    }
+    if (!acceptedTypes.filter((x) => x.toLocaleLowerCase().includes(attach.name.toLowerCase()))) {
+      this.container.logger.warn(`${attach.name.toLowerCase()} Types: ${acceptedTypes.join(', ')}`);
+      // prettier-ignore
+      await interaction.reply({embeds: [EmbedCreator.Error('__No Valid File Found!__\r\n>>> The selected message must include a valid log type!\r\n- RagePluginHook.log\r\n- ELS.log\r\n- ScriptHookVDotNet.log\r\n- asiloader.log\r\n- .xml\r\n- .meta')], ephemeral: true});
+      return;
+    }
+
+    if (attach.name.toLowerCase().includes('ragepluginhook')) {
+      await interaction.reply({ embeds: [EmbedCreator.Success('__Validating!__\r\n>>> The file is currently being processed. Please wait...')] });
+      const rphProc = new RPHValidator();
+      const log = await rphProc.validate(attach.url);
+
+      // prettier-ignore
+      await interaction.editReply(`Log proocessed in ${log.elapsedTime}ms\r\nUp To Date Plugins: ${log.current.length}\r\nOutdated Plugins: ${log.outdated.length}\r\nErrors: ${log.errors.length}`);
     }
   }
 }
