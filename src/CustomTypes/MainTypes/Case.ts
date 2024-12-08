7f4321b60252640cc4de77101c7252724594f236
SuperPyroManiac <1407314+SuperPyroManiac@users.noreply.github.com>
2024-12-07 19:16:24 -0700

    feat: Implement Case class
    - Add `Case` class to represent a case entity
    - Include properties for id, owner, channel, server, open status, creation date, and expiration date
    - Implement `clone()` method to create a deep copy of the `Case` instance

---
+++ src/CustomTypes/MainTypes/Case.ts
@@ -0,0 +1,19 @@
+import { addDays } from 'date-fns';
+
+export class Case {
+  id!: string;
+  ownerId?: string;
+  channelId?: string;
+  serverId?: string;
+  open: boolean = true;
+  createDate: Date = new Date();
+  expireDate: Date = addDays(new Date(), 1);
+
+  constructor(id: string = '') {
+    this.id = id;
+  }
+
+  clone(): Case {
+    return Object.assign(new Case(), JSON.parse(JSON.stringify(this)));
+  }
+}
