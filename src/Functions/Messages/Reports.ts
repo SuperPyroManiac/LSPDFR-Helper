a2bd0ba2b4936ff5d5ce1b07682a1750e91d3363
SuperPyroManiac <1407314+SuperPyroManiac@users.noreply.github.com>
2024-12-11 21:53:27 -0700

    fix: update user log channel to use correct environment variable
    - Update `Logger.UserLog` to use the `USER_LOG_CHANNEL` environment variable instead of `BOT_LOG_CHANNEL`
    - Handle case where attachment size is too large to upload

--- src/Functions/Messages/Reports.ts
+++ src/Functions/Messages/Reports.ts
@@ -5,84 +5,83 @@ import { Cache } from '../../Cache';
 import { UsersValidation } from '../Validations/Users';
 import { DBManager } from '../DBManager';
 
+type ReportInteraction = MessageContextMenuCommandInteraction | Message | StringSelectMenuInteraction;
+
+const SUPPORT_SERVER_URL = 'https://dsc.PyrosFun.com';
+const ERROR_MESSAGES = {
+  MODIFIED: `__Modified Log__\n>>> -# This log is invalid!\nThe selected log has been modified and will not be processed.\nFor more information you can join the bots support server at ${SUPPORT_SERVER_URL}`,
+  OVERSIZED: `__Oversized Log__\n>>> -# This log is not allowed!\nThis log is bigger than 3MB and will not be processed.\nFor more information you can join the bots support server at ${SUPPORT_SERVER_URL}`,
+  OVERSIZED_BAN: `__Oversized Log__\n>>> -# This log is not allowed!\nThis log is much bigger than 3MB and will not be processed.\nDue to the size, you have been automatically banned. For more information you can join the bots support server at ${SUPPORT_SERVER_URL}`,
+};
+
 export abstract class Reports {
-  static async modifiedLog(interaction: MessageContextMenuCommandInteraction | Message | StringSelectMenuInteraction, attach: Attachment) {
+  static async modifiedLog(interaction: ReportInteraction, attach: Attachment): Promise<void> {
     if (interaction instanceof StringSelectMenuInteraction) {
       await interaction.editReply({
-        embeds: [
-          EmbedCreator.Error(
-            '__Modified Log__\r\n>>> ' +
-              '-# This log is invalid!\r\n' +
-              'The selected log has been modified and will not be processed.\r\n' +
-              'For more information you can join the bots support server at https://dsc.PyrosFun.com'
-          ),
-        ],
+        embeds: [EmbedCreator.Error(ERROR_MESSAGES.MODIFIED)],
       });
     } else {
       await interaction.reply({
-        embeds: [
-          EmbedCreator.Error(
-            '__Modified Log__\r\n>>> ' +
-              '-# This log is invalid!\r\n' +
-              'The selected log has been modified and will not be processed.\r\n' +
-              'For more information you can join the bots support server at https://dsc.PyrosFun.com'
-          ),
-        ],
+        embeds: [EmbedCreator.Error(ERROR_MESSAGES.MODIFIED)],
         ephemeral: true,
       });
     }
     await this.sendAbuseMsg('Modified log', interaction, attach);
   }
 
-  static async largeLog(interaction: MessageContextMenuCommandInteraction | Message, attach: Attachment, extra = false) {
+  static async largeLog(interaction: ReportInteraction, attach: Attachment, extra = false): Promise<void> {
+    const fileSize = attach.size / 1000000;
+
     if (extra) {
-      await interaction.reply({
-        embeds: [
-          EmbedCreator.Error(
-            '__Oversized Log__\r\n>>> ' +
-              '-# This log is not allowed!\r\n' +
-              'This log is much bigger than 3MB and will not be processed.\r\n' +
-              'Due to the size, you have been automatically banned.' +
-              'For more information you can join the bots support server at https://dsc.PyrosFun.com'
-          ),
-        ],
+      const response = {
+        embeds: [EmbedCreator.Error(ERROR_MESSAGES.OVERSIZED_BAN)],
         ephemeral: true,
-      });
-      const user = Cache.getUser(interaction.member?.user.id!);
-      if (!user) UsersValidation.AddMissing();
-      if (user) {
+      };
+
+      if (interaction instanceof StringSelectMenuInteraction) {
+        await interaction.editReply(response);
+      } else {
+        await interaction.reply(response);
+      }
+
+      const userId = interaction.member?.user.id;
+      const user = userId ? Cache.getUser(userId) : null;
+
+      if (!user) {
+        await UsersValidation.AddMissing();
+      } else {
         user.banned = true;
         await DBManager.editUser(user);
       }
     } else {
-      await interaction.reply({
-        embeds: [
-          EmbedCreator.Error(
-            '__Oversized Log__\r\n>>> ' +
-              '-# This log is not allowed!\r\n' +
-              'This log is bigger than 3MB and will not be processed.\r\n' +
-              'For more information you can join the bots support server at https://dsc.PyrosFun.com'
-          ),
-        ],
+      const response = {
+        embeds: [EmbedCreator.Error(ERROR_MESSAGES.OVERSIZED)],
         ephemeral: true,
-      });
+      };
+
+      if (interaction instanceof StringSelectMenuInteraction) {
+        await interaction.editReply(response);
+      } else {
+        await interaction.reply(response);
+      }
     }
+
     await this.sendAbuseMsg('Oversized log', interaction, attach);
-    return;
   }
 
-  private static async sendAbuseMsg(reason: string, interaction: MessageContextMenuCommandInteraction | Message | StringSelectMenuInteraction, attach: Attachment) {
+  static async sendAbuseMsg(reason: string, interaction: ReportInteraction, attach: Attachment): Promise<void> {
     let chName = 'User CMD';
     if (interaction.channel && !interaction.channel.isDMBased()) chName = interaction.channel.name;
-    const emb = EmbedCreator.Alert(
-      '__Possible Abuse__\r\n>>> ' +
-        `**User:** ${interaction.member?.user.username} (${interaction.member?.user.id})\r\n` +
-        `**Server:** ${interaction.guild?.name} (${interaction.guild?.id})\r\n` +
-        `**Channel:** ${chName} (${interaction.channel?.id})\r\n` +
-        `**Log Size:** ${(attach.size / 1000000).toFixed(2)}MB\r\n` +
-        `**Reason:** ${reason}\r\n`
-    );
-    emb.setDescription(emb.data.description?.replaceAll('undefined', 'User CMD')!);
+    const fileSize = (attach.size / 1000000).toFixed(2);
+
+    const embedDescription = `__Possible Abuse__\n>>>
+        **User:** ${interaction.member?.user.username ?? 'Unknown'} (${interaction.member?.user.id ?? 'Unknown'})\n
+        **Server:** ${interaction.guild?.name ?? 'DM'} (${interaction.guild?.id ?? 'N/A'})\n
+        **Channel:** ${chName} (${interaction.channel?.id ?? 'N/A'})\n
+        **Log Size:** ${fileSize}MB\n
+        **Reason:** ${reason}\n`;
+
+    const emb = EmbedCreator.Alert(embedDescription);
     await Logger.UserLog(emb, attach);
   }
 }
